b6fe1440bdfb5f0353efa7bcac7c571e0b32c331
reshmavaase <reshv@RESH>
2024-03-18 16:28:32 -0500

    Angular setup

--- TravelHub-frontend/src/app/app.module.ts
+++ TravelHub-frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './sharedpages/navbar/navbar.component';
+import { FootbarComponent } from './sharedpages/footbar/footbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent,
+    NavbarComponent,
+    FootbarComponent
   ],
   imports: [
     BrowserModule,
