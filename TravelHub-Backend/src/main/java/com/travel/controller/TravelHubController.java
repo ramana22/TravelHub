225fc0650715d17b8db8b3e4262e708482227719
reshmavaase <reshv@RESH>
2024-03-19 18:35:32 -0500

    Backend chnages for review

--- TravelHub-Backend/src/main/java/com/travel/controller/TravelHubController.java
+++ TravelHub-Backend/src/main/java/com/travel/controller/TravelHubController.java
@@ -1,11 +1,15 @@
 package com.travel.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.travel.model.Review;
 import com.travel.model.User;
 import com.travel.service.TravelHubService;
 
