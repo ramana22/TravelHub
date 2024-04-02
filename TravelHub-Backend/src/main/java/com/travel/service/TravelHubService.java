f301dce9d4b1d591f874317e075e111b2773af0b
reshmavaase5 <reshmavaase557@gmail.com>
2024-04-02 16:23:14 -0500

    changes for hotel data storing ,fetching and many

--- TravelHub-Backend/src/main/java/com/travel/service/TravelHubService.java
+++ TravelHub-Backend/src/main/java/com/travel/service/TravelHubService.java
@@ -6,12 +6,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import com.travel.model.Bus;
+import com.travel.model.Car;
+import com.travel.model.Hotel;
 import com.travel.model.Review;
+import com.travel.model.Room;
 import com.travel.model.Train;
 import com.travel.model.User;
 import com.travel.repository.BusRepository;
+import com.travel.repository.CarRepository;
+import com.travel.repository.HotelRepository;
 import com.travel.repository.ReviewRepository;
+import com.travel.repository.RoomRepository;
 import com.travel.repository.TrainRepository;
 import com.travel.repository.UserRepository;
 
@@ -25,6 +32,8 @@ public class TravelHubService {
 	TrainRepository trainrepo;
 	@Autowired
 	BusRepository busrepo;
+	@Autowired
+	CarRepository carrepo;
 	public User saveUser(User user) throws Exception {
 		if(user==null) {
 			throw new Exception("user is null");
@@ -55,5 +64,26 @@ public class TravelHubService {
 	 public List<Bus> searchBuses(String departureTerminal, String arrivalTerminal, LocalDate departureDate) {
 	        return busrepo.findByDepartureTerminalStartingWithIgnoreCaseAndArrivalTerminalStartingWithIgnoreCaseAndDepartureDate(departureTerminal, arrivalTerminal, departureDate);
 	 }
+	 public Car saveCar(Car car) {
+		 return carrepo.save(car);
+	 }
+	 public List<Car> searchCars(String pickupLocation, LocalDate rentalStartDate, LocalDate rentalEndDate) {
+		    List<Car> cars = carrepo.findByPickupLocationAndRentalStartDateLessThanEqualAndRentalEndDateGreaterThanEqual(
+		            pickupLocation, rentalStartDate, rentalEndDate);
+		    return cars;
+	}
+	@Autowired
+	private RoomRepository roomrepo;
+	@Autowired
+	private HotelRepository hotelrepo;
+	public Hotel saveHotel(Hotel hotel) {
+		return hotelrepo.save(hotel);
+	}
+	public Room saveRoom(Room room) {
+		return roomrepo.save(room);
+	}
+	public List<Hotel> getAllHotels() {
+        return hotelrepo.findAll();
+    }
 	 
 }
