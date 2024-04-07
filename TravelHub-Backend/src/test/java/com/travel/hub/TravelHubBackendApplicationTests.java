22ef26394794e0cf84e63a3273cdbbcc7db79664
Ramana06 <ramana@coolkaru.com>
2024-04-07 18:36:03 -0500

    initial commit empty branch

--- TravelHub-Backend/src/test/java/com/travel/hub/TravelHubBackendApplicationTests.java
+++ TravelHub-Backend/src/test/java/com/travel/hub/TravelHubBackendApplicationTests.java
@@ -2,7 +2,7 @@ package com.travel.hub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test commit
 @SpringBootTest
 class TravelHubBackendApplicationTests {
 
