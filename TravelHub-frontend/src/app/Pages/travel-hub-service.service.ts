61956874d98d081c8ddb26e33d6236dd1fe4fca1
ramana22 <52375631+ramana22@users.noreply.github.com>
2024-04-28 14:25:01 -0500

    Update travel-hub-service.service.ts

--- TravelHub-frontend/src/app/Pages/travel-hub-service.service.ts
+++ TravelHub-frontend/src/app/Pages/travel-hub-service.service.ts
@@ -18,10 +18,10 @@ export class TravelHubServiceService {
 
   public registerUserFromRemote(user: User): Observable<any> {
     this.currentuser = user;
-    return this.http.post(`/register`, user);
+    return this.http.post(`https://travel.up.railway.app/register`, user);
   }
   public loginUserFromRemote(user: User): Observable<any> {
-    return this.http.post(`/login`, user).pipe(
+    return this.http.post(`https://travel.up.railway.app/login`, user).pipe(
       tap(() => {
         // Assuming this.getUser returns an Observable
         this.setCurrentUser(user);
@@ -32,10 +32,10 @@ export class TravelHubServiceService {
   }
   public saveprofile(profile: Profile): Observable<any> {
     console.log(profile)
-    return this.http.post(`/saveprofile`, profile);
+    return this.http.post(`https://travel.up.railway.app/saveprofile`, profile);
   }
   getProfile(email: string): Observable<any> {
-    return this.http.get(`/getprofile?email=${email}`);
+    return this.http.get(`https://travel.up.railway.app/getprofile?email=${email}`);
   }
   
   setCurrentUser(user: User): void {
@@ -45,13 +45,13 @@ export class TravelHubServiceService {
     return this.currentuser;
   }
   public getReviews(): Observable<any> {
-    return this.http.get(`/getreview`);
+    return this.http.get(`https://travel.up.railway.app/getreview`);
   }
   saveReview(review: Review): Observable<any> {
-    return this.http.post<any>(`/saveReview`, review);
+    return this.http.post<any>(`https://travel.up.railway.app/saveReview`, review);
   }
   searchFlights(departureAirport: string, arrivalAirport: string, departureDate: string, returnDate: string, passengers : number): Observable<any> {
-    const url = `/searchFlights`;
+    const url = `https://travel.up.railway.app/searchFlights`;
 
     // Construct the query parameters
     const params = {
@@ -66,7 +66,7 @@ export class TravelHubServiceService {
     return this.http.get(url, { params });
   }
   searchFlightsoneway(departureAirport: string, arrivalAirport: string, departureDate: string, passengers : number): Observable<any> {
-    const url = `/searchFlightsoneway`;
+    const url = `https://travel.up.railway.app/searchFlightsoneway`;
 
     // Construct the query parameters
     const params = {
@@ -80,7 +80,7 @@ export class TravelHubServiceService {
     return this.http.get(url, { params });
   }
   getAirports(keyword: string) {
-    return this.http.get<any[]>(`/searchLocations?keyword=${keyword}`);
+    return this.http.get<any[]>(`https://travel.up.railway.app/searchLocations?keyword=${keyword}`);
   }
  
   searchBuses(departureTerminal: string, arrivalTerminal: string,departureDate:string) {
@@ -89,7 +89,7 @@ export class TravelHubServiceService {
       arrivalTerminal,
       departureDate
     };
-    return this.http.post<Bus[]>(`/buses`, searchParams);
+    return this.http.post<Bus[]>(`https://travel.up.railway.app/buses`, searchParams);
   }
   searchTrains(departureStation: string, arrivalStation: string,departureDate:Date) {
     const searchParams = {
@@ -97,10 +97,10 @@ export class TravelHubServiceService {
       arrivalStation,
       departureDate
     };
-    return this.http.post<Train[]>(`/trains`, searchParams);
+    return this.http.post<Train[]>(`https://travel.up.railway.app/trains`, searchParams);
   }
   searchDestination(destination: string): Observable<any> {
-    return this.http.post<any>(`/getfilterlist`, { city: destination });
+    return this.http.post<any>(`https://travel.up.railway.app/getfilterlist`, { city: destination });
   }
   public getCars(pickupLocation:string,rentalStartDate:string,rentalEndDate:string):Observable<any> {
     const searchParams = {
@@ -108,18 +108,18 @@ export class TravelHubServiceService {
       rentalStartDate,
       rentalEndDate
     };
-    return this.http.post("/cars",searchParams);
+    return this.http.post("https://travel.up.railway.app/cars",searchParams);
   }
 
   saveBusTicket(busTicket: BusTicket,userEmail:string){
-    return this.http.post<BusTicket>(`/saveBusTicket?userEmail=${userEmail}`, busTicket);
+    return this.http.post<BusTicket>(`https://travel.up.railway.app/saveBusTicket?userEmail=${userEmail}`, busTicket);
   }
 
   saveTrainTicket(trainTicket: TrainTicket,userEmail:string): Observable<TrainTicket> {
-    return this.http.post<TrainTicket>(`/saveTrainTicket?userEmail=${userEmail}`, trainTicket);
+    return this.http.post<TrainTicket>(`https://travel.up.railway.app/saveTrainTicket?userEmail=${userEmail}`, trainTicket);
   }
   saveFlightTicket(flightTicket: FlightTicket,userEmail:string): Observable<TrainTicket> {
-    return this.http.post<TrainTicket>(`/saveFlightTicket?userEmail=${userEmail}`, flightTicket);
+    return this.http.post<TrainTicket>(`https://travel.up.railway.app/saveFlightTicket?userEmail=${userEmail}`, flightTicket);
   }
 
   saveHotelBooking(hotelBooking: HotelBooking,userEmail:string){
@@ -133,7 +133,7 @@ export class TravelHubServiceService {
       userEmail: userEmail
     };
 
-    return this.http.post<CarBooking>(`/savecarBooking`, requestBody);
+    return this.http.post<CarBooking>(`https://travel.up.railway.app/savecarBooking`, requestBody);
   }
   setSelectedFlight(flight: any) {
     this.selectedFlight = flight;
@@ -143,19 +143,19 @@ export class TravelHubServiceService {
     return this.selectedFlight;
   }
   getRestaurants(destination: string): Observable<any> {
-    return this.http.post<any>(`/getfilterRestaurant`, { city: destination });
+    return this.http.post<any>(`https://travel.up.railway.app/getfilterRestaurant`, { city: destination });
   }
   changePassword(userEmail:string, oldPassword: string, newPassword: string): Observable<any> {
-    const url = `/change`;
+    const url = `https://travel.up.railway.app/change`;
     const body = {userEmail, oldpass: oldPassword, newpass: newPassword };
     return this.http.post<any>(url, body);
   }
   getBookings(email: string): Observable<any> {
     const body = { email: email };
-    return this.http.post<any>(`/getBooking`, body);
+    return this.http.post<any>(`https://travel.up.railway.app/getBooking`, body);
   }
   getflighttickets(email: string): Observable<any> {
     const body = { email: email };
-    return this.http.post<any>(`/getflightticket`, body);
+    return this.http.post<any>(`https://travel.up.railway.app/getflightticket`, body);
   }
 }
