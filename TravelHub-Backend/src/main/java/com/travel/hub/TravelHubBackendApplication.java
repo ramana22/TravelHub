0567f899fc2a193c943cf5c69b12c1a476182bc0
Ramana06 <ramana@coolkaru.com>
2024-04-28 21:59:50 -0500

    cross-origing issue fix

--- TravelHub-Backend/src/main/java/com/travel/hub/TravelHubBackendApplication.java
+++ TravelHub-Backend/src/main/java/com/travel/hub/TravelHubBackendApplication.java
@@ -12,17 +12,30 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import jakarta.annotation.PostConstruct;
 @SpringBootApplication(scanBasePackages = "com.travel.hub")
 @EnableJpaRepositories(basePackages = "com.travel.repository")
 @EntityScan(basePackages = "com.travel.model")
 @ComponentScan(basePackages = "com.travel")
-public class TravelHubBackendApplication {
+public class TravelHubBackendApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(TravelHubBackendApplication.class, args);
 	}
 	@Autowired
     private DataSource dataSource;
+
+     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow requests from any origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                .allowedHeaders("*"); // Allowed headers
+    }
+
 
     @PostConstruct
     public void testConnection() {
