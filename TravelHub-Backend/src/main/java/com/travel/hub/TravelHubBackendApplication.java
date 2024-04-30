8bb3d73447ce43c569a66f14eaeca531d7c65737
reshmavaase5 <reshmavaase557@gmail.com>
2024-04-30 00:43:27 -0500

    chnages

--- TravelHub-Backend/src/main/java/com/travel/hub/TravelHubBackendApplication.java
+++ TravelHub-Backend/src/main/java/com/travel/hub/TravelHubBackendApplication.java
@@ -1,22 +1,13 @@
 package com.travel.hub;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import jakarta.annotation.PostConstruct;
 @SpringBootApplication(scanBasePackages = "com.travel.hub")
 @EnableJpaRepositories(basePackages = "com.travel.repository")
 @EntityScan(basePackages = "com.travel.model")
@@ -25,26 +16,12 @@ public class TravelHubBackendApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(TravelHubBackendApplication.class, args);
 	}
-	@Autowired
-    private DataSource dataSource;
 
-     @Override
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*") // Allow requests from any origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
                 .allowedHeaders("*"); // Allowed headers
-    }
-
-
-    @PostConstruct
-    public void testConnection() {
-        try {
-            Connection connection = dataSource.getConnection();
-            System.out.println("Connected to MySQL!");
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+    }  
 }
